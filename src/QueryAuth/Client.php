18eb848d66988c86f810eefe849ff257583f511b
Jeremy Kendall <jeremy@jeremykendall.net>
2013-08-03 16:32:24 -0500

    Adds method, host, path, and params to signature

--- src/QueryAuth/Client.php
+++ src/QueryAuth/Client.php
@@ -5,30 +5,57 @@ namespace QueryAuth;
 class Client
 {
     /**
-     * Generate signature
+     * Sign request params
      *
-     * @param  string $key       API key
-     * @param  string $secret    API secret
-     * @param  int    $timestamp Unix timestamp
-     * @return string Signature
+     * @param  string $key    API key
+     * @param  string $secret API secret
+     * @param  string $method Request method (GET, POST, PUT, HEAD, etc)
+     * @param  string $host   Host portion of API resource URL (including subdomain, excluding scheme)
+     * @param  string $path   Path portion of API resource URL (excluding query and fragment)
+     * @param  array  $params OPTIONAL Request params (query or POST fields), only needed if required by endpoint
+     * @return array  Signed request params
      */
-    public function generateSignature($key, $secret, $timestamp)
+    public function getSignedRequestParams($key, $secret, $method, $host, $path, array $params = array())
     {
-        return \base64_encode(
-            \hash_hmac('sha256', $key . $timestamp, $secret, true)
-        );
+        $params['key'] = $key;
+        $params['timestamp'] = (int) gmdate('U');
+
+        // Ensure path is absolute
+        $path = '/' . ltrim($path, '/');
+
+        $data = $method . "\n"
+            . $host . "\n"
+            . $path . "\n"
+            . $this->getNormalizedParameterString($params);
+
+        $signature = \base64_encode(\hash_hmac('sha256', $data, $secret, true));
+
+        if ($method == 'GET') {
+            $signature = urlencode($signature);
+        }
+
+        $params['signature'] = $signature;
+
+        return $params;
     }
 
     /**
-     * Generate URL encoded signature
+     * Get request params as string, sorted alphabetically using strcmp
      *
-     * @param  string $key       API key
-     * @param  string $secret    API secret
-     * @param  int    $timestamp Unix timestamp
-     * @return string Signature
+     * @param  array  $params Array of request paramaters
+     * @return string Normalized, rawurlencoded parameters as string
      */
-    public function generateUrlEncodedSignature($key, $secret, $timestamp)
+    public function getNormalizedParameterString(array $params)
     {
-        return urlencode($this->generateSignature($key, $secret, $timestamp));
+        unset($params['signature']);
+        uksort($params, 'strcmp');
+
+        $normalized = '';
+
+        foreach ($params as $key => $value) {
+            $normalized .= rawurlencode($key) . '=' . rawurlencode($value) . '&';
+        }
+
+        return substr($normalized, 0, -1);
     }
 }
--- tests/QueryAuth/Tests/ClientTest.php
+++ tests/QueryAuth/Tests/ClientTest.php
@@ -26,48 +26,98 @@ class ClientTest extends \PHPUnit_Framework_TestCase
      */
     private $timestamp;
 
+    /**
+        * @var string Base 64 regex
+     */
+    private $base64Pattern;
+
     protected function setUp()
     {
+        $this->base64Pattern = '/^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)$/';
         $this->client = new Client();
         $this->key = md5(time());
         $this->secret = base64_encode(time() . 'secret');
-        $this->timestamp = time();
+        $this->host = 'www.example.com';
+        $this->path = '/resources';
     }
 
-    public function testGenerateSignature()
+    /**
+     * This would be an example of a GET request that should be authenticated
+     * but has no required or optional params of its own.
+     *
+     * http://www.example.com/resources
+     *
+     * @covers QueryAuth\Client::getSignedRequestParams()
+     */
+    public function testGetSignedRequestParamsForGetRequestWithoutParams()
     {
-        $signature = $this->client->generateSignature(
-            $this->key, $this->secret, $this->timestamp
+        $result = $this->client->getSignedRequestParams(
+            $this->key, $this->secret, 'GET', $this->host, $this->path, $params = array()
         );
 
-        $this->assertRegexp('/^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)$/', $signature);
+        $this->assertInternalType('array', $result);
+        $this->assertNotEmpty($result);
+        $this->assertArrayHasKey('timestamp', $result);
+        $this->assertArrayHasKey('key', $result);
+        $this->assertArrayHasKey('signature', $result);
+        $this->assertEquals(3, count($result));
     }
 
+    /**
+     * @covers QueryAuth\Client::getSignedRequestParams()
+     */
+    public function testGetRequestSignatureIsUrlEncoded()
+    {
+        $result = $this->client->getSignedRequestParams(
+            $this->key, $this->secret, 'GET', $this->host, $this->path, $params = array()
+        );
+
+        $encodedSignature = $result['signature'];
+        $signature = urldecode($encodedSignature);
+        $this->assertRegexp($this->base64Pattern, $signature);
+    }
+
+    /**
+     * @covers QueryAuth\Client::getSignedRequestParams()
+     */
+    public function testNotGetRequestSignatureIsNotUrlEncoded()
+    {
+        $result = $this->client->getSignedRequestParams(
+            $this->key, $this->secret, 'POST', $this->host, $this->path, $params = array('bar' => 'baz')
+        );
+
+        $this->assertRegexp($this->base64Pattern, $result['signature']);
+    }
+
+    /**
+     * @covers QueryAuth\Client::getSignedRequestParams()
+     */
     public function testSameDataGeneratesSameSignatures()
     {
-        $sig1 = $this->client->generateSignature(
-            $this->key, $this->secret, $this->timestamp
+        $result1 = $this->client->getSignedRequestParams(
+            $this->key, $this->secret, 'POST', $this->host, $this->path, $params = array('bar' => 'baz')
         );
 
-        $sig2 = $this->client->generateSignature(
-            $this->key, $this->secret, $this->timestamp
+        $result2 = $this->client->getSignedRequestParams(
+            $this->key, $this->secret, 'POST', $this->host, $this->path, $params = array('bar' => 'baz')
         );
 
-        $this->assertEquals($sig1, $sig2);
+        $this->assertEquals($result1['signature'], $result2['signature']);
     }
 
-    public function testGenerateUrlEncodedSignature()
+    public function testGetNormalizedParameterString()
     {
-        $encoded = $this->client->generateUrlEncodedSignature(
-            $this->key, $this->secret, $this->timestamp
-        );
+        $params = array('email' => 'sam@example.com', 'name' => 'Sam Jones', 'comment' => "Y'all rock!");
+        $expected = 'comment=Y%27all%20rock%21&email=sam%40example.com&name=Sam%20Jones';
+        $actual = $this->client->getNormalizedParameterString($params);
 
-        $signature = $this->client->generateSignature(
-            $this->key, $this->secret, $this->timestamp
-        );
+        $this->assertEquals($expected, $actual);
+    }
 
-        // Since the same data generates the same sig, the decoded url encoded
-        // signature should match a normal signature
-        $this->assertEquals($signature, urldecode($encoded));
+    public function testGetNormalizedParameterStringDoesNotOperateOnSignature()
+    {
+        $params = array('signature' => 'John Hancock');
+        $result = $this->client->getNormalizedParameterString($params);
+        $this->assertEquals('', $result);
     }
 }
