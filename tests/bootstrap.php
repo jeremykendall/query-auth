1cf9d9948b8b0c24384f11aa713cd2a6a01953d3
Jeremy Kendall <jeremy@jeremykendall.net>
2013-08-19 07:20:48 -0500

    Tests die with warning if 'composer install' hasn't been run

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 date_default_timezone_set('UTC');
 
+$autoloader = realpath(__DIR__ . '/../vendor/autoload.php');
+
+if (!file_exists($autoloader)) {
+    $message = 'Composer generated autoloader does not exist. '
+      . " Please run 'composer install' before running tests.";
+    die($message . PHP_EOL);
+}
+
 $loader = require realpath(__DIR__ . '/../vendor/autoload.php');
 $loader->add('QueryAuth\\', __DIR__);
 
