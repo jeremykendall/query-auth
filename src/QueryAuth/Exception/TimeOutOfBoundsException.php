fda5cb5693f118ff77dfe41099525170bd6e3274
Jeremy Kendall <jeremy@jeremykendall.net>
2013-08-22 21:03:04 -0500

    Adds phpdoc documentation

--- src/QueryAuth/Exception/TimeOutOfBoundsException.php
+++ src/QueryAuth/Exception/TimeOutOfBoundsException.php
@@ -1,7 +1,17 @@
 <?php
+/**
+ * Query Auth: Signature generation and validation for REST API query authentication
+ *
+ * @copyright 2013 Jeremy Kendall
+ * @license https://github.com/jeremykendall/query-auth/blob/master/LICENSE MIT
+ * @link https://github.com/jeremykendall/query-auth
+ */
 
 namespace QueryAuth\Exception;
 
+/**
+ * Thrown when request timestamp is out of allowable clock drift
+ */
 class TimeOutOfBoundsException extends \OutOfBoundsException
 {
 }
