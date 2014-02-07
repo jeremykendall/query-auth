b8c971ef8d22c5e538aa50e222a2fada6b180397
Jeremy Kendall <jeremy@jeremykendall.net>
2014-02-06 19:04:47 -0600

    Updates copyright

--- src/QueryAuth/Factory.php
+++ src/QueryAuth/Factory.php
@@ -2,7 +2,7 @@
 /**
  * Query Auth: Signature generation and validation for REST API query authentication
  *
- * @copyright 2013 Jeremy Kendall
+ * @copyright 2013-2014 Jeremy Kendall
  * @license https://github.com/jeremykendall/query-auth/blob/master/LICENSE MIT
  * @link https://github.com/jeremykendall/query-auth
  */
