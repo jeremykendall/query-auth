933320d4d5b6f27d89d11eade2bbca1095897105
jeremykendall <jeremy@jeremykendall.net>
2014-09-19 08:14:33 -0500

    Adds credentials class and interface

---
+++ tests/QueryAuth/Tests/Credentials/CredentialsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace QueryAuthTests\Credentials;
+
+use QueryAuth\Credentials\Credentials;
+
+class CredentialsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCredentials()
+    {
+        $key = 'not so secret API key';
+        $secret = 'super secret API secret';
+
+        $credentials = new Credentials($key, $secret);
+
+        $this->assertInstanceOf(
+            'QueryAuth\Credentials\CredentialsInterface', 
+            $credentials
+        );
+        $this->assertEquals($key, $credentials->getKey());
+        $this->assertEquals($secret, $credentials->getSecret());
+    }
+}
