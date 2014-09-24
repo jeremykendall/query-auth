f93b296db65a6e2b695c30442a63a2efa3b63d88
jeremykendall <jeremy@jeremykendall.net>
2014-09-24 18:38:48 -0500

    New exception

---
+++ src/QueryAuth/Exception/TimestampMissingException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Query Auth: Signature generation and validation for REST API query authentication
+ *
+ * @copyright 2013-2014 Jeremy Kendall
+ * @license https://github.com/jeremykendall/query-auth/blob/master/LICENSE MIT
+ * @link https://github.com/jeremykendall/query-auth
+ */
+
+namespace QueryAuth\Exception;
+
+/**
+ * Exception thrown when timestamp param is missing from request
+ */
+class TimestampMissingException extends \Exception implements QueryAuthException
+{
+}
